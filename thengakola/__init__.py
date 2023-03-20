27e027ce1184bc9b4d0443963699d40a30a2db49
NAVEEN GEORGE JOSEPH <naveengjoseph@gmail.com>
2023-03-20 12:51:13 +0530

    adapt to pythonanywhere

--- thengakola/__init__.py
+++ thengakola/__init__.py
@@ -4,28 +4,31 @@ from flask import Flask, redirect, url_for
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY=open("secret.txt").read())
-
+    app.config.from_mapping(
+        SECRET_KEY=open("/home/ngj14/thengakola-2.0/secret.txt").read()
+    )
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
-
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    from . import db
+    from thengakola import db
 
     db.init_app(app)
 
-    from . import auth
+    from thengakola import auth
 
     app.register_blueprint(auth.bp)
 
-    from . import views
+    from thengakola import views
 
     app.register_blueprint(views.bp)
 
     return app
+
+
+app = create_app()
