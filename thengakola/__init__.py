8abc7406835e83f5228bc6012a83b2863023d1c1
Rohan Jacob <rohan.rj120@gmail.com>
2023-03-20 14:02:24 +0530

    remove hardcoded path

--- thengakola/__init__.py
+++ thengakola/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask, redirect, url_for
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY=open("/home/ngj14/thengakola-2.0/secret.txt").read()
+        SECRET_KEY=open("secret.txt").read()
     )
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
